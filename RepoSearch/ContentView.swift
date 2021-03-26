558c5d5c48c51497cfba627be0429826d9c6177b
Jedrzej <jedrzej.kus@icloud.com>
2021-03-26 09:47:18 +0100

    Initial Commit

---
+++ RepoSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RepoSearch
+//
+//  Created by Jędrzej Kuś on 26/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RepoSearch/RepoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoSearchApp.swift
+//  RepoSearch
+//
+//  Created by Jędrzej Kuś on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct RepoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
